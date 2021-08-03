4f82614654100f6589bb3d9cdd3fa8d507e5d658
Maxim424 <mvk0402@gmail.com>
2021-08-03 16:46:40 +0300

    1) Сделано "окно" GameOver 2) В UnitCollection добавлено 2 метода

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -1,6 +1,8 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
 
 public class GamePanel extends JPanel implements MouseListener, MouseMotionListener, MouseWheelListener, KeyListener {
 
@@ -52,22 +54,25 @@ public class GamePanel extends JPanel implements MouseListener, MouseMotionListe
             if (keyState.keyDown(KeyEvent.VK_DOWN) || keyState.keyDown(KeyEvent.VK_S)) {
                 player.down();
                 player.setSpeed(100);
-                player.update(15);
+                player.update(ms);
             } else if (keyState.keyDown(KeyEvent.VK_UP) || keyState.keyDown(KeyEvent.VK_W)) {
                 player.up();
                 player.setSpeed(100);
-                player.update(15);
+                player.update(ms);
             } else if (keyState.keyDown(KeyEvent.VK_LEFT) || keyState.keyDown(KeyEvent.VK_A)) {
                 player.left();
                 player.setSpeed(100);
-                player.update(15);
+                player.update(ms);
             } else if (keyState.keyDown(KeyEvent.VK_RIGHT) || keyState.keyDown(KeyEvent.VK_D)) {
                 player.right();
                 player.setSpeed(100);
-                player.update(15);
+                player.update(ms);
             } else {
                 player.setSpeed(0);
-                player.update(15);
+                player.update(ms);
+            }
+            if (keyState.keyDown(KeyEvent.VK_SPACE)) {
+                UnitCollection.spawnBullet(player.getX(), player.getY(), player.top.getAlpha(), ms);
             }
         }
         keyState.update();
@@ -82,121 +87,25 @@ public class GamePanel extends JPanel implements MouseListener, MouseMotionListe
 
         if (!mapEditor.isActive()) {
             UnitCollection.update(ms);
-
-            if (System.currentTimeMillis()%3000<=50) {
-                if (UnitCollection.list.get(0).active) {
-                    UnitCollection.spawnBullet(UnitCollection.list.get(0));
-                }
-                if (UnitCollection.list.get(1).active) {
-                    UnitCollection.spawnBullet(UnitCollection.list.get(1));
-                }
-                if (UnitCollection.list.get(2).active) {
-                    UnitCollection.spawnBullet(UnitCollection.list.get(2));
-                }
-                if (UnitCollection.list.get(3).active) {
-                    UnitCollection.spawnBullet(UnitCollection.list.get(3));
-                }
-                if (UnitCollection.list.get(4).active) {
-                    UnitCollection.spawnBullet(UnitCollection.list.get(0));
-                }
-                if (UnitCollection.list.get(5).active) {
-                    UnitCollection.spawnBullet(UnitCollection.list.get(5));
-                }
-                if (UnitCollection.list.get(6).active) {
-                    UnitCollection.spawnBullet(UnitCollection.list.get(6));
-                }
-                if (UnitCollection.list.get(7).active) {
-                    UnitCollection.spawnBullet(UnitCollection.list.get(7));
-                }
-                if (UnitCollection.list.get(8).active) {
-                    UnitCollection.spawnBullet(UnitCollection.list.get(8));
-                }
-            }
-            if (System.currentTimeMillis()%2000<=50) {
-                if (UnitCollection.list.get(0).active) {
-                    int rnd = (int)(Math.random()*5 + 4);
-                    if (UnitCollection.list.get(rnd).active) {
-                        UnitCollection.list.get(0).bot.target = UnitCollection.list.get(rnd);
-                    }
-
-                }
-                if (UnitCollection.list.get(1).active) {
-                    int rnd = (int)(Math.random()*5 + 4);
-                    if (UnitCollection.list.get(rnd).active) {
-                        UnitCollection.list.get(1).bot.target = UnitCollection.list.get(rnd);
-                    }
-                }
-                if (UnitCollection.list.get(2).active) {
-                    int rnd = (int)(Math.random()*5 + 4);
-                    if (UnitCollection.list.get(rnd).active) {
-                        UnitCollection.list.get(2).bot.target = UnitCollection.list.get(rnd);
-                    }
-                }
-                if (UnitCollection.list.get(3).active) {
-                    int rnd = (int)(Math.random()*5 + 4);
-                    if (UnitCollection.list.get(rnd).active) {
-                        UnitCollection.list.get(3).bot.target = UnitCollection.list.get(rnd);
-                    }
-                }
-                if (UnitCollection.list.get(4).active) {
-                    int rnd = (int)(Math.random()*4);
-                    if (UnitCollection.list.get(rnd).active) {
-                        UnitCollection.list.get(4).bot.target = UnitCollection.list.get(rnd);
-                    }
-                }
-                if (UnitCollection.list.get(5).active) {
-                    int rnd = (int)(Math.random()*4);
-                    if (UnitCollection.list.get(rnd).active) {
-                        UnitCollection.list.get(5).bot.target = UnitCollection.list.get(rnd);
-                    }
-                }
-                if (UnitCollection.list.get(6).active) {
-                    int rnd = (int)(Math.random()*4);
-                    if (UnitCollection.list.get(rnd).active) {
-                        UnitCollection.list.get(6).bot.target = UnitCollection.list.get(rnd);
-                    }
-                }
-                if (UnitCollection.list.get(7).active) {
-                    int rnd = (int)(Math.random()*4);
-                    if (UnitCollection.list.get(rnd).active) {
-                        UnitCollection.list.get(7).bot.target = UnitCollection.list.get(rnd);
-                    }
-                }
-                if (UnitCollection.list.get(8).active) {
-                    int rnd = (int)(Math.random()*4);
-                    if (UnitCollection.list.get(rnd).active) {
-                        UnitCollection.list.get(8).bot.target = UnitCollection.list.get(rnd);
-                    }
-                }
-            }
-
-
+            UnitCollection.spawnBullets();
+            UnitCollection.setTarget();
             camera.setPosition(player.getX() - getWidth() / 2.0 - 16, player.getY() - getHeight() / 2.0 - 16, getWidth(), getHeight());
         } else{
             point.update(ms);
             camera.setPosition(point.getX(), point.getY(), getWidth(), getHeight());
         }
-
         if (!player.active) {
             endgame = true;
         }
-
-
-        t1 = t2;
         keyState.update();
         mapEditor.update(ms);
-
-        if (keyState.keyDown(KeyEvent.VK_SPACE)) {
-            UnitCollection.spawnBullet(player.getX(), player.getY(), player.top.getAlpha(), ms);
-        }
+        controlPlayer(ms);
+        t1 = t2;
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        int ms = (int) (t2 - t1);
-        controlPlayer(ms);
-
         map.paint(g);
         UnitCollection.paint(g);
         player.paint(g);
@@ -205,11 +114,23 @@ public class GamePanel extends JPanel implements MouseListener, MouseMotionListe
             update();
         }
         else {
-            g.setColor(Color.RED);
-            g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 50));
-            g.drawString("Your tank has been destroyed.", w/2 - 200, h/2);
-            g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 22));
-            g.drawString("Press [esc] to exit to the main menu.", w/2 - 100, h/2 + 60);
+
+            if (player.active) {
+
+
+            }
+            else {
+                try {
+                    g.drawImage(ImageIO.read(new File("src/GameOver.png")), w/2 - 360, h/2 - 202, null);
+                }
+                catch (Exception e) {
+                    g.setColor(Color.RED);
+                    g.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 50));
+                    g.drawString("Your tank has been destroyed.", w/2 - 200, h/2);
+                    g.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 22));
+                    g.drawString("Press [esc] to exit to the main menu.", w/2 - 100, h/2 + 60);
+                }
+            }
         }
 
 
