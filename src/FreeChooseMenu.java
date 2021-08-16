685cf033ede0b777dcf7f9ba8281b4ab72d00116
Maxim424 <mvk0402@gmail.com>
2021-08-16 17:50:24 +0300

    Доработка меню 2

--- src/FreeChooseMenu.java
+++ src/FreeChooseMenu.java
@@ -55,15 +55,15 @@ public class FreeChooseMenu extends JDialog {
         contentPane.setLayout(new com.intellij.uiDesigner.core.GridLayoutManager(3, 1, new Insets(10, 10, 10, 10), -1, -1));
         contentPane.setBackground(new Color(-1382436));
         final JLabel label1 = new JLabel();
-        label1.setText("Загрузка карты");
+        label1.setText("Map");
         contentPane.add(label1, new com.intellij.uiDesigner.core.GridConstraints(0, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_NONE, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         NewMapButton = new JButton();
         NewMapButton.setBackground(new Color(-1474444));
-        NewMapButton.setText("Новая карта");
+        NewMapButton.setText("New map");
         contentPane.add(NewMapButton, new com.intellij.uiDesigner.core.GridConstraints(1, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         LoadMapButton = new JButton();
         LoadMapButton.setBackground(new Color(-1474444));
-        LoadMapButton.setText("Загрузить карту");
+        LoadMapButton.setText("Load map");
         contentPane.add(LoadMapButton, new com.intellij.uiDesigner.core.GridConstraints(2, 0, 1, 1, com.intellij.uiDesigner.core.GridConstraints.ANCHOR_CENTER, com.intellij.uiDesigner.core.GridConstraints.FILL_HORIZONTAL, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_SHRINK | com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_CAN_GROW, com.intellij.uiDesigner.core.GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
     }
 
@@ -73,4 +73,5 @@ public class FreeChooseMenu extends JDialog {
     public JComponent $$$getRootComponent$$$() {
         return contentPane;
     }
+
 }
